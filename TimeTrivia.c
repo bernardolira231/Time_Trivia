2ec76b81cecaa966ea0913f33a10732a0757a0cd
Bernardo Lira <harritolira@gmail.com>
2021-11-22 15:22:19 -0600

    repair samurai function

--- TimeTrivia.c
+++ TimeTrivia.c
@@ -58,6 +58,8 @@ void seleccion_de_lugar(char *nombre){
 
 void jardin_botanico(int *score){
 
+    int *score2;
+    score2 = score;
     int opc = 0, repeat = 0;
     printf ("Viajaste al Jardin Botanico en el año 800, ¿Con quien decide hablar?\n");
     printf("1.- Samurai\n2.- Gariboy");
@@ -65,7 +67,7 @@ void jardin_botanico(int *score){
     scanf("%d", &opc);
 
     if(opc == 1){
-        samurai(&score);
+        samurai(&score2);
         repeat = 1;
     }
     if(opc == 2){
@@ -80,7 +82,7 @@ void jardin_botanico(int *score){
         scanf("%d", &opc);
 
         if(opc == 1){
-            samurai(&score);
+            samurai(&score2);
             repeat = 1;
         }
         if(opc == 2){
@@ -101,7 +103,6 @@ void tvu(){}
 void samurai(int *score){
 
     int obj = 0, respuesta = 0, repeat = 0;
-    printf("%d\n", *score);
 
     printf("Hola viajero veo que escogiste venir al jardin botanico, en este lugar tu mision sera encontrar partes de mi armadura que perdi en la batalla contra el gariclan, para poder recoger los objetos que veas tendras que resolver un acertijo.\nBuscando por el jardin encontraste estos objetos cual quieres recoger");
     printf("\n1.- Cactus \n2.- Katana \n3.- Piedra\n");
@@ -117,12 +118,113 @@ void samurai(int *score){
         if(respuesta == 1){
             printf("Has acertado ganas un punto\n");
             *score = *score + 1;
+            repeat = 2;
         }
 
         if(respuesta == 2 || respuesta == 3){
             printf("Esa no es la respuesta correcta pierdes un punto");
             repeat = 1;
+            *score = *score - 1;
         }
+
+        if(repeat == 2){
+            printf("Respondiste correctamente, sin embargo ese objeto no es el que perdi, por favor recoge otro\n");
+            printf("\n2.- Katana \n3.- Piedra\n");
+            printf("Cual recogeras: ");
+            scanf("%d", &obj);
+
+            if(obj == 2){
+                printf("¿Cual es el codigo etico de los samurai?");
+                printf("\n1) Takataka\n2) El derecho al respeto ajeno es la paz\n3) Bushido\n");
+                printf("R: ");
+                scanf("%d", &respuesta);
+
+                if(respuesta == 3){
+                    printf("Has acertado ganas un punto\n");
+                    repeat = 2;
+                }
+
+                if(respuesta == 2 || respuesta == 1){
+                    printf("Esa no es la respuesta correcta pierdes un punto");
+                    repeat = 1;
+                }
+
+                while(repeat == 1){
+                    printf("¬øCual es el codigo etico de los samurai?");
+                    printf("1) Takataka\n2) El derecho al respeto ajeno es la paz\n3) Bushido");
+                    printf("R: ");
+                    scanf("%d", &respuesta);
+
+                    if(respuesta == 3){
+                        printf("Has acertado ganas un punto\n");
+                        repeat = 0;
+                    }
+
+                    if(respuesta == 2 || respuesta == 1){
+                        printf("Esa no es la respuesta correcta pierdes un punto");
+                        repeat = 1;
+                    }           
+                }
+            }
+
+            if(obj == 3){
+                printf("¿Que le dice un techo a otro techo?\n");
+                printf("1) Te echo de menos\n2) Somos tejanos\n3) Somos iguales\n");
+                printf("R: ");
+                scanf("%d", &respuesta);
+
+                if(respuesta == 1){
+                    printf("Has acertado ganas un punto\n");
+                    repeat = 2;
+                }
+
+                if(respuesta == 2 || respuesta == 3){
+                    printf("Esa no es la respuesta correcta pierdes un punto");
+                    repeat = 1;
+                }
+
+                if(repeat == 2){
+                    printf("Respondiste correctamente, sin embargo ese objeto no es el que perdi, por favor recoge otro\n");
+                    printf("\n2.- Katana\n");
+                    printf("Cual recogeras: ");
+                    scanf("%d", &obj); 
+
+                    if(obj == 2){
+                        printf("¿Cual es el codigo etico de los samurai?");
+                        printf("\n1) Takataka\n2) El derecho al respeto ajeno es la paz\n3) Bushido\n");
+                        printf("R: ");
+                        scanf("%d", &respuesta);
+
+                        if(respuesta == 3){
+                            printf("Has acertado ganas un punto\n");
+                            repeat = 2;
+                        }
+
+                        if(respuesta == 2 || respuesta == 1){
+                            printf("Esa no es la respuesta correcta pierdes un punto");
+                            repeat = 1;
+                        }
+
+                        while(repeat == 1){
+                            printf("¿Cual es el codigo etico de los samurai?");
+                            printf("1) Takataka\n2) El derecho al respeto ajeno es la paz\n3) Bushido");
+                            printf("R: ");
+                            scanf("%d", &respuesta);
+
+                            if(respuesta == 3){
+                                printf("Has acertado ganas un punto\n");
+                                repeat = 0;
+                            }
+
+                            if(respuesta == 2 || respuesta == 1){
+                                printf("Esa no es la respuesta correcta pierdes un punto");
+                                repeat = 1;
+                            }           
+                        }
+                    }
+                }
+            }
+
         while(repeat == 1){
             printf("\n¿Cuantas personas caben en una manguera?\n");
             printf("1) Un chorro\n2) 3.1416\n3) 299.792,458 kilómetros por segundo\n");
@@ -142,7 +244,7 @@ void samurai(int *score){
     }
     if(obj == 2){
         printf("¿Cual es el codigo etico de los samurai?");
-        printf("1) Takataka\n2) El derecho al respeto ajeno es la paz\n3) Bushido");
+        printf("\n1) Takataka\n2) El derecho al respeto ajeno es la paz\n3) Bushido\n");
         printf("R: ");
         scanf("%d", &respuesta);
 
@@ -206,5 +308,6 @@ void samurai(int *score){
                 repeat = 1;
             }
         }
+    }
     }
 }
